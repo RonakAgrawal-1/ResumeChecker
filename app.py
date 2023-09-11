979cb9a4a462ebba727ba90d0d4f4694ba65a476
ronak <ronaka2328@gmail.com>
2023-09-11 21:52:02 +0530

    v16

--- app.py
+++ app.py
@@ -127,7 +127,6 @@ uploaded_file = st.sidebar.file_uploader("Upload your resume (PDF or DOC/DOCX)")
 st.sidebar.subheader("Step 2: Enter Job Description")
 job_description_text = st.sidebar.text_area("Enter the job description")
 
-
 # Analyze Button in Sidebar
 if st.sidebar.button("Analyze"):
     if not uploaded_file or not job_description_text:
